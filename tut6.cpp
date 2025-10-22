8c33eeaf5ce071f6cf11ac73e796cb412e7c9b64
tanmaykapil17 <tanmaykapil.in@gmail.com>
2025-10-22 14:07:48 +0530

    added code

--- tut6.cpp
+++ tut6.cpp
@@ -14,12 +14,34 @@ int main() {
         
         ans = (bit * pow( 10 , i)) + ans ;
         
-        n= n>>1;
+           n= n>>1;
          i++;
          
        
    }
      cout <<" The answer is : "<< ans <<endl; 
+
+   int n;
+    cin>>n;
+     int i=0 , ans=0;
+     while (n!=0)
+     {
+        int digit= n%10;
+        if (digit==1)
+        {
+            ans = ans + pow ( 2, i);
+
+        }
+        
+        n= n/10;
+        i++;
+     }
+     
+     cout<<ans<<endl;
+
+
     return 0;
 }
 
+
+            
